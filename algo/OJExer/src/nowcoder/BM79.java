3a5ed55fdf657187f0f6f7b8ce29dfe3277f4f0a
WangTao <1609832760@qq.com>
2023-08-21 08:37:43 +0800

    动态规划练习

--- algo/OJExer/src/nowcoder/BM79.java
+++ algo/OJExer/src/nowcoder/BM79.java
@@ -6,4 +6,25 @@ package nowcoder;
  * @Date 2023/8/17 10:23
  */
 public class BM79 {
+    public int rob (int[] nums) {
+        // write code
+        int n = nums.length;
+        return Math.max(rob1(nums, 2, n - 2) + nums[0], rob1(nums, 1, n - 1));
+    }
+
+    public int rob1(int[] nums, int left, int right){
+        if(left > right){
+            return 0;
+        }
+        int n = nums.length;
+        int[] f = new int[n];
+        int[] g = new int[n];
+        f[left] = nums[left];
+        for(int i = left + 1; i <= right; i++){
+            f[i] = g[i-1] + nums[i];
+            g[i] = Math.max(g[i-1], f[i-1]);
+        }
+
+        return Math.max(f[right], g[right]);
+    }
 }
