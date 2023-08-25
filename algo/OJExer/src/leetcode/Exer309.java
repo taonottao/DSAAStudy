c293b61b290452e02cdbf8355f300f54f5e9e527
WangTao <1609832760@qq.com>
2023-08-25 08:24:07 +0800

    OJ 练习

---
+++ algo/OJExer/src/leetcode/Exer309.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+/**
+ * 买卖股票的最佳时机含冷冻期
+ * @version 1.0
+ * @Author T-WANG
+ * @Date 2023/8/24 12:17
+ */
+public class Exer309 {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[][] dp = new int[n][3];
+        dp[0][0] = -prices[0];
+        for(int i = 1; i < n; i++){
+            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] - prices[i]);
+            dp[i][1] = Math.max(dp[i-1][2], dp[i-1][1]);
+            dp[i][2] = dp[i-1][0] + prices[i];
+        }
+        return Math.max(dp[n-1][1], dp[n-1][2]);
+    }
+}
