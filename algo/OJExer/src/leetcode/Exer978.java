bb41197e66d77e3ff1e899927aeb1ece33c4cf6a
WangTao <1609832760@qq.com>
2023-08-30 07:54:01 +0800

    OJ 练习

--- algo/OJExer/src/leetcode/Exer978.java
+++ algo/OJExer/src/leetcode/Exer978.java
@@ -1,5 +1,9 @@
 package leetcode;
 
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Scanner;
+
 /**
  * 最长湍流子数组
  * @version 1.0
@@ -7,6 +11,41 @@ package leetcode;
  * @Date 2023/8/28 12:26
  */
 public class Exer978 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        // 注意 hasNext 和 hasNextLine 的区别
+        while (in.hasNextLine()) { // 注意 while 处理多个 case
+            String str = in.nextLine();
+            Deque<String> stack = new LinkedList<>();
+            for(int i = 0; i <str.length(); i++){
+                char ch = str.charAt(i);
+                if((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')){
+                    int left= i;
+                    int right = i;
+                    while(i < str.length() && (ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')){
+                        right++;
+                        i++;
+                        if(i < str.length()){
+                            ch = str.charAt(i);
+                        }else {
+                            break;
+                        }
+                    }
+                    String subStr = str.substring(left, right);
+                    stack.push(subStr);
+                    i--;
+                }else{
+                    if(!stack.peek().equals(" ")){
+                        stack.push(" ");
+                    }
+                }
+            }
+            while(!stack.isEmpty()){
+                System.out.print(stack.pop());
+            }
+            System.out.println();
+        }
+    }
     public int maxTurbulenceSize(int[] arr) {
         int n = arr.length;
         int[] f = new int[n];
