285ce92fd08c7bb8c7178c3861bfbb3acac3e834
WangTao <1609832760@qq.com>
2023-11-03 09:10:40 +0800

    OJ 练习

--- algo/OJExer/src/leetcode/prefix_sum/Exer560.java
+++ algo/OJExer/src/leetcode/prefix_sum/Exer560.java
@@ -1,5 +1,7 @@
 package leetcode.prefix_sum;
 
+import java.util.HashMap;
+
 /**
  * 和为 K 的子数组
  * @version 1.0
@@ -7,4 +9,17 @@ package leetcode.prefix_sum;
  * @Date 2023/11/1 10:45
  */
 public class Exer560 {
+    public int subarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> hash = new HashMap<>();
+        hash.put(0, 1);
+        int sum = 0;
+        int ret = 0;
+
+        for(int i : nums){
+            sum += i;
+            ret += hash.getOrDefault(sum - k, 0);
+            hash.put(sum, hash.getOrDefault(sum, 0) + 1);
+        }
+        return ret;
+    }
 }
